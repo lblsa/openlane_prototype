3c22da35f5693a13028144dc3765659aa165868d
Roman Efimushkin <roman.efimushkin@gmail.com>
2013-10-22 15:04:42 +0400

    Add IComponentPtr

--- include/openlane/component.h
+++ include/openlane/component.h
@@ -1,12 +1,16 @@
 #ifndef COMPONENT_H
 #define COMPONENT_H
 
+#include <ptr.hpp>
+
 namespace openlane {
 
 class IComponent {
   public:
     virtual ~IComponent() = 0;
 };
+
+typedef smart::ptr<IComponent> IComponentPtr;
 
 } /* openlane */
 
